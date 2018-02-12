ab1b9e0c904f01cb4d7fbd6261bacf565df8c3ad
sNNNeaker <31590991+sNNNeaker@users.noreply.github.com>
2018-02-12 08:39:06 +0100

    Shutdown to own repo

--- shutdown/shutdown.bat
+++
@@ -1,2 +0,0 @@
-echo ### Shutdown Timer and Shutdown ###
-timeout -t 297 && shutdown.exe -s -t 3
--- shutdown/start.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-echo ### Start Shutdown ###
-start .\shutdown.bat
-echo ### Start User Interface ###
-start .\index.hta
-exit
